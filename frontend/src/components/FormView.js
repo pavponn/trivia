1377d6058b8e5ccd81d2b6b7e9bbe2396f73e16b
Pavel Ponomarev <pavponn@yandex.ru>
2020-09-05 11:57:05 +0300

    Cleanup frontend

--- frontend/src/components/FormView.js
+++ frontend/src/components/FormView.js
@@ -17,8 +17,7 @@ class FormView extends Component {
 
   componentDidMount(){
     $.ajax({
-      url: `/categories`, //TODO: update request URL
-      type: "GET",
+      url: `/categories`,
       success: (result) => {
         this.setState({ categories: result.categories })
         return;
@@ -34,7 +33,7 @@ class FormView extends Component {
   submitQuestion = (event) => {
     event.preventDefault();
     $.ajax({
-      url: '/questions', //TODO: update request URL
+      url: '/questions',
       type: "POST",
       dataType: 'json',
       contentType: 'application/json',
